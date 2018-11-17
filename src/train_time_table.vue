fca355c36c713ce3ab05343dd10cb55406d17421
Madao <chrishyman256@gmail.com>
2018-11-17 09:15:40 +0800

    add files

--- src/train_time_table.vue
+++ src/train_time_table.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-  <div class='__vue-train-timetbale'>
-    <ul :class='"timetable " + directionClass'>
+  <div :class='"__vue-train-timetbale " + directionClass'>
+    <ul :style='directionStyle' :class='"timetable " + directionClass' ref="timetable">
       <li v-for='stop, index in displayList' class='stop-item'>
         <i :class='"stop-bar " + appendBarClass(stop)' :style='barBackground'>
           <i class='start-point' :style='barBackground' v-if='is_start(stop)'></i>
@@ -74,6 +74,16 @@ export default {
   watch: {},
 
   computed: {
+    directionStyle() {
+      let timetableEl = this.$refs.timetable;
+      const result =
+        this.direction == "vertical"
+          ? {}
+          : {
+              width: this.displayList.length * 25 + "px"
+            };
+      return result;
+    },
     directionClass() {
       return this.direction == "vertical" ? "vertical" : "horizontal";
     },
@@ -121,12 +131,15 @@ export default {
 @point_size: 12px;
 @bar_width: 15px;
 @bar_text_padding: 20px;
-
+@item_height: 25px;
 .__vue-train-timetbale {
   font-family: helvetica, sans-serif;
   position: relative;
   font-weight: 500;
   box-sizing: border-box;
+  &.horizontal {
+    // overflow-x: hidden;
+  }
   .start-point,
   .end-point {
     width: @point_size;
@@ -144,60 +157,80 @@ export default {
   }
   .stop-item {
     padding: 5px 0 5px @bar_text_padding;
+    box-sizing: border-box;
     position: relative;
-    .stop-bar {
-      position: absolute;
-      width: 5px;
-      height: 100%;
-      top: 0;
-      left: 0;
-      &.start {
-        top: 50%;
-        height: 50%;
-        .pointing {
-          top: 0;
-        }
+    height: @item_height;
+  }
+  .stop-bar {
+    position: absolute;
+  }
+  .timetable {
+    &.horizontal {
+      .start-point {
+        top: -4px;
       }
-      &.end {
-        bottom: 50%;
-        height: 50%;
-        .pointing {
-          top: 100%;
-        }
-      }
-      &.pointing {
+      .stop-bar {
+        width: 100%;
         height: 4px;
-        width: @bar_width;
-        top: 50%;
+        top: 4px;
         left: 0;
-        margin-top: -2px;
-        // transform: translateX(50%);
+        &.pointing {
+          top: 0;
+          width: 4px;
+          height: 10px;
+          position: absolute;
+          left: 0;
+        }
+      }
+      .stop-item {
+        float: left;
+        position: relative;
+        width: @item_height;
+        height: auto;
+      }
+      .stop-text {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        transform: rotateZ(90deg) translateX(2px);
+        transform-origin: 0;
+        height: 20px;
+        width: 100%;
+        min-width: 120px;
+      }
+    }
+
+    &.vertical {
+      .stop-bar {
+        width: 5px;
+        height: 100%;
+        top: 0;
+        left: 0;
+        &.start {
+          top: 50%;
+          height: 50%;
+          .pointing {
+            top: 0;
+          }
+        }
+        &.end {
+          bottom: 50%;
+          height: 50%;
+          .pointing {
+            top: 100%;
+          }
+        }
+        &.pointing {
+          height: 4px;
+          width: @bar_width;
+          top: 50%;
+          left: 0;
+          margin-top: -2px;
+          // transform: translateX(50%);
+        }
       }
     }
   }
-  .horizontal.timetable {
-    transform-origin: center;
-    transform: rotate(-90deg);
-    // translateX(0%) translateY(50%);
-  }
-  // .horizontal.timetable {
-  //   .stop-item {
-  //     display: inline-block;
-  //     width: 13px;
-  //     padding: 0 3px;
-  //     height: 100%;
-  //     .stop-text {
-  //       display: inline;
-  //       word-break: break-all;
-  //     }
-  //     .stop-bar {
-  //       top: auto;
-  //       left: 0;
-  //       bottom: 0;
-  //       // transform: rotate(90deg);
-  //     }
-  //   }
-  // }
 }
 .stop-text {
   font-size: 14px;
