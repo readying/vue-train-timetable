ec62fdb0bfbd423149a0cc1fc6a42027b35e1a3f
Madao <chrishyman256@gmail.com>
2018-11-20 00:29:15 +0800

    packing up version 0.1.1

--- dist/vue-train-timetable.js
+++ dist/vue-train-timetable.js
@@ -1,5 +1,5 @@
 /*!
- * vue-train-timetable v0.1.0
+ * vue-train-timetable v0.1.1
  * (c) 2018-present Madao <chrishyman256@gmail.com>
  * Released under the MIT License.
  */
@@ -9,6 +9,7 @@
 	(factory((global.vueTrainTimetable = {})));
 }(this, (function (exports) { 'use strict';
 
+//
 //
 //
 //
@@ -82,13 +83,16 @@ var script = {
     return {};
   },
 
-  watch: {},
-
   computed: {
+    // bearingWallText() {
+    //   return this.direction == "vertical" ? "" : (this.timetable.map(function(item) {
+    //     return item.name
+    //   })[0]
+    // },
     directionStyle: function directionStyle() {
       var timetableEl = this.$refs.timetable;
       var result = this.direction == "vertical" ? {} : {
-        width: this.displayList.length * 25 + "px"
+        width: this.displayList.length * 50 + "px"
       };
       return result;
     },
@@ -124,12 +128,18 @@ var script = {
     appendBarClass: function appendBarClass(data) {
       var list = this.displayList;
       var index = list.indexOf(data);
+      var classNames = [];
       if (index == list.length - 1) {
-        return "end";
+        classNames.push("end");
       } else if (index == 0) {
-        return "start";
+        classNames.push("start");
       }
-      return "";
+      if (this.is_start(data)) {
+        classNames.push("departure");
+      } else if (this.is_end(data)) {
+        classNames.push("terminal");
+      }
+      return classNames.join(" ");
     }
   }
 };
@@ -148,10 +158,7 @@ var __vue_render__ = function () {
     class: "timetable " + _vm.directionClass,
     style: _vm.directionStyle
   }, _vm._l(_vm.displayList, function (stop, index) {
-    return _c("li", { staticClass: "stop-item" }, [_c("i", {
-      class: "stop-bar " + _vm.appendBarClass(stop),
-      style: _vm.barBackground
-    }, [_vm.is_start(stop) ? _c("i", {
+    return _c("li", { class: "stop-item " + _vm.appendBarClass(stop) }, [_c("i", { staticClass: "stop-bar", style: _vm.barBackground }, [_vm.is_start(stop) ? _c("i", {
       staticClass: "start-point",
       style: _vm.barBackground
     }) : _vm.is_end(stop) ? _c("i", {
@@ -169,10 +176,10 @@ __vue_render__._withStripped = true;
 /* style */
 var __vue_inject_styles__ = function (inject) {
   if (!inject) { return; }
-  inject("data-v-f0b15d8a_0", { source: "\n.__vue-train-timetbale[data-v-f0b15d8a] {\n  font-family: helvetica, sans-serif;\n  position: relative;\n  font-weight: 500;\n  box-sizing: border-box;\n}\n.__vue-train-timetbale ul li[data-v-f0b15d8a] {\n  list-style: none;\n}\n.__vue-train-timetbale .start-point[data-v-f0b15d8a],\n.__vue-train-timetbale .end-point[data-v-f0b15d8a] {\n  width: 12px;\n  height: 12px;\n  border-radius: 12px;\n  position: absolute;\n  left: 0;\n  margin-left: -3px;\n}\n.__vue-train-timetbale .start-point[data-v-f0b15d8a] {\n  top: -6px;\n}\n.__vue-train-timetbale .end-point[data-v-f0b15d8a] {\n  bottom: -6px;\n}\n.__vue-train-timetbale .stop-item[data-v-f0b15d8a] {\n  padding: 5px 0 5px 20px;\n  box-sizing: border-box;\n  position: relative;\n  height: 25px;\n}\n.__vue-train-timetbale .stop-bar[data-v-f0b15d8a] {\n  position: absolute;\n}\n.__vue-train-timetbale .timetable.horizontal .start-point[data-v-f0b15d8a] {\n  top: -4px;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-bar[data-v-f0b15d8a] {\n  width: 100%;\n  height: 4px;\n  top: 4px;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-bar.pointing[data-v-f0b15d8a] {\n  top: 0;\n  width: 4px;\n  height: 10px;\n  position: absolute;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-item[data-v-f0b15d8a] {\n  float: left;\n  position: relative;\n  width: 25px;\n  height: auto;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-text[data-v-f0b15d8a] {\n  position: absolute;\n  bottom: 0;\n  left: 0;\n  transform: rotateZ(90deg) translateX(2px);\n  transform-origin: 0;\n  height: 20px;\n  width: 100%;\n  min-width: 120px;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar[data-v-f0b15d8a] {\n  width: 5px;\n  height: 100%;\n  top: 0;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.start[data-v-f0b15d8a] {\n  top: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.start .pointing[data-v-f0b15d8a] {\n  top: 0;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.end[data-v-f0b15d8a] {\n  bottom: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.end .pointing[data-v-f0b15d8a] {\n  top: 100%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.pointing[data-v-f0b15d8a] {\n  height: 4px;\n  width: 15px;\n  top: 50%;\n  left: 0;\n  margin-top: -2px;\n}\n.stop-text[data-v-f0b15d8a] {\n  font-size: 14px;\n}\n", map: { "version": 3, "sources": ["vue-train-timetable.vue"], "names": [], "mappings": ";AAAA;EACE,mCAAmC;EACnC,mBAAmB;EACnB,iBAAiB;EACjB,uBAAuB;CACxB;AACD;EACE,iBAAiB;CAClB;AACD;;EAEE,YAAY;EACZ,aAAa;EACb,oBAAoB;EACpB,mBAAmB;EACnB,QAAQ;EACR,kBAAkB;CACnB;AACD;EACE,UAAU;CACX;AACD;EACE,aAAa;CACd;AACD;EACE,wBAAwB;EACxB,uBAAuB;EACvB,mBAAmB;EACnB,aAAa;CACd;AACD;EACE,mBAAmB;CACpB;AACD;EACE,UAAU;CACX;AACD;EACE,YAAY;EACZ,YAAY;EACZ,SAAS;EACT,QAAQ;CACT;AACD;EACE,OAAO;EACP,WAAW;EACX,aAAa;EACb,mBAAmB;EACnB,QAAQ;CACT;AACD;EACE,YAAY;EACZ,mBAAmB;EACnB,YAAY;EACZ,aAAa;CACd;AACD;EACE,mBAAmB;EACnB,UAAU;EACV,QAAQ;EACR,0CAA0C;EAC1C,oBAAoB;EACpB,aAAa;EACb,YAAY;EACZ,iBAAiB;CAClB;AACD;EACE,WAAW;EACX,aAAa;EACb,OAAO;EACP,QAAQ;CACT;AACD;EACE,SAAS;EACT,YAAY;CACb;AACD;EACE,OAAO;CACR;AACD;EACE,YAAY;EACZ,YAAY;CACb;AACD;EACE,UAAU;CACX;AACD;EACE,YAAY;EACZ,YAAY;EACZ,SAAS;EACT,QAAQ;EACR,iBAAiB;CAClB;AACD;EACE,gBAAgB;CACjB", "file": "vue-train-timetable.vue", "sourcesContent": [".__vue-train-timetbale {\n  font-family: helvetica, sans-serif;\n  position: relative;\n  font-weight: 500;\n  box-sizing: border-box;\n}\n.__vue-train-timetbale ul li {\n  list-style: none;\n}\n.__vue-train-timetbale .start-point,\n.__vue-train-timetbale .end-point {\n  width: 12px;\n  height: 12px;\n  border-radius: 12px;\n  position: absolute;\n  left: 0;\n  margin-left: -3px;\n}\n.__vue-train-timetbale .start-point {\n  top: -6px;\n}\n.__vue-train-timetbale .end-point {\n  bottom: -6px;\n}\n.__vue-train-timetbale .stop-item {\n  padding: 5px 0 5px 20px;\n  box-sizing: border-box;\n  position: relative;\n  height: 25px;\n}\n.__vue-train-timetbale .stop-bar {\n  position: absolute;\n}\n.__vue-train-timetbale .timetable.horizontal .start-point {\n  top: -4px;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-bar {\n  width: 100%;\n  height: 4px;\n  top: 4px;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-bar.pointing {\n  top: 0;\n  width: 4px;\n  height: 10px;\n  position: absolute;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-item {\n  float: left;\n  position: relative;\n  width: 25px;\n  height: auto;\n}\n.__vue-train-timetbale .timetable.horizontal .stop-text {\n  position: absolute;\n  bottom: 0;\n  left: 0;\n  transform: rotateZ(90deg) translateX(2px);\n  transform-origin: 0;\n  height: 20px;\n  width: 100%;\n  min-width: 120px;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar {\n  width: 5px;\n  height: 100%;\n  top: 0;\n  left: 0;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.start {\n  top: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.start .pointing {\n  top: 0;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.end {\n  bottom: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.end .pointing {\n  top: 100%;\n}\n.__vue-train-timetbale .timetable.vertical .stop-bar.pointing {\n  height: 4px;\n  width: 15px;\n  top: 50%;\n  left: 0;\n  margin-top: -2px;\n}\n.stop-text {\n  font-size: 14px;\n}\n"] }, media: undefined });
+  inject("data-v-c487f6fa_0", { source: "\n.__vue-train-timetbale[data-v-c487f6fa] {\n  font-family: helvetica, sans-serif;\n  position: relative;\n  font-weight: 500;\n  box-sizing: border-box;\n}\n.__vue-train-timetbale ul[data-v-c487f6fa] {\n  padding: 0;\n  margin: 0;\n}\n.__vue-train-timetbale ul li[data-v-c487f6fa] {\n  list-style: none;\n}\n.__vue-train-timetbale .timetable[data-v-c487f6fa] {\n  height: 100%;\n}\n.__vue-train-timetbale .start-point[data-v-c487f6fa],\n.__vue-train-timetbale .end-point[data-v-c487f6fa] {\n  width: 12px;\n  height: 12px;\n  border-radius: 12px;\n  position: absolute;\n  left: 0;\n  margin-left: -3px;\n}\n.__vue-train-timetbale .start-point[data-v-c487f6fa] {\n  top: -6px;\n}\n.__vue-train-timetbale .end-point[data-v-c487f6fa] {\n  bottom: -6px;\n}\n.__vue-train-timetbale .stop-item[data-v-c487f6fa] {\n  padding: 5px 0 5px 20px;\n  box-sizing: border-box;\n  position: relative;\n  height: 25px;\n}\n.__vue-train-timetbale .stop-bar[data-v-c487f6fa] {\n  position: absolute;\n}\n.__vue-train-timetbale .bearing-wall[data-v-c487f6fa] {\n  position: relative;\n  display: block;\n  visibility: hidden;\n  width: 0.1px;\n  text-indent: -1000px;\n  word-break: break-all;\n  line-height: 0.6;\n}\n.__vue-train-timetbale.horizontal[data-v-c487f6fa] {\n  height: 100%;\n}\n.__vue-train-timetbale.horizontal .start-point[data-v-c487f6fa] {\n  top: -4px;\n}\n.__vue-train-timetbale.horizontal .stop-bar[data-v-c487f6fa] {\n  width: 100%;\n  height: 4px;\n  bottom: 0px;\n  left: 0;\n}\n.__vue-train-timetbale.horizontal .stop-bar.pointing[data-v-c487f6fa] {\n  bottom: 0;\n  width: 4px;\n  height: 10px;\n  position: absolute;\n  left: 0;\n}\n.__vue-train-timetbale.horizontal .stop-item[data-v-c487f6fa] {\n  float: left;\n  position: relative;\n  width: 50px;\n  height: 100%;\n}\n.__vue-train-timetbale.horizontal .stop-text[data-v-c487f6fa] {\n  position: absolute;\n  bottom: 0;\n  left: 0;\n  transform: rotateZ(-45deg) translateY(-5px) translateX(10px);\n  transform-origin: left;\n  text-align: left;\n  width: 140px;\n}\n.__vue-train-timetbale.horizontal .departure .stop-text[data-v-c487f6fa] {\n  transform: rotateZ(-45deg) translateY(-15px) translateX(-10px);\n}\n.__vue-train-timetbale.horizontal .end-point[data-v-c487f6fa] {\n  bottom: -3px;\n}\n.__vue-train-timetbale.horizontal .terminal .stop-bar[data-v-c487f6fa] {\n  width: 0;\n}\n.__vue-train-timetbale.vertical .stop-item.start .stop-bar[data-v-c487f6fa] {\n  top: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale.vertical .stop-item.start .stop-bar .pointing[data-v-c487f6fa] {\n  top: 0;\n}\n.__vue-train-timetbale.vertical .stop-item.end .stop-bar[data-v-c487f6fa] {\n  bottom: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale.vertical .stop-item.end .stop-bar .pointing[data-v-c487f6fa] {\n  top: 100%;\n}\n.__vue-train-timetbale.vertical .stop-item .departure .stop-text[data-v-c487f6fa] {\n  top: -50px;\n  left: 0;\n  transform: rotateZ(20deg) translateX(-30%);\n  transform-origin: left;\n  max-width: 140px;\n  text-align: left;\n}\n.__vue-train-timetbale.vertical .stop-bar[data-v-c487f6fa] {\n  width: 5px;\n  height: 100%;\n  top: 0;\n  left: 0;\n}\n.__vue-train-timetbale.vertical .stop-bar.pointing[data-v-c487f6fa] {\n  height: 4px;\n  width: 15px;\n  top: 50%;\n  left: 0;\n  margin-top: -2px;\n}\n", map: { "version": 3, "sources": ["vue-train-timetable.vue"], "names": [], "mappings": ";AAAA;EACE,mCAAmC;EACnC,mBAAmB;EACnB,iBAAiB;EACjB,uBAAuB;CACxB;AACD;EACE,WAAW;EACX,UAAU;CACX;AACD;EACE,iBAAiB;CAClB;AACD;EACE,aAAa;CACd;AACD;;EAEE,YAAY;EACZ,aAAa;EACb,oBAAoB;EACpB,mBAAmB;EACnB,QAAQ;EACR,kBAAkB;CACnB;AACD;EACE,UAAU;CACX;AACD;EACE,aAAa;CACd;AACD;EACE,wBAAwB;EACxB,uBAAuB;EACvB,mBAAmB;EACnB,aAAa;CACd;AACD;EACE,mBAAmB;CACpB;AACD;EACE,mBAAmB;EACnB,eAAe;EACf,mBAAmB;EACnB,aAAa;EACb,qBAAqB;EACrB,sBAAsB;EACtB,iBAAiB;CAClB;AACD;EACE,aAAa;CACd;AACD;EACE,UAAU;CACX;AACD;EACE,YAAY;EACZ,YAAY;EACZ,YAAY;EACZ,QAAQ;CACT;AACD;EACE,UAAU;EACV,WAAW;EACX,aAAa;EACb,mBAAmB;EACnB,QAAQ;CACT;AACD;EACE,YAAY;EACZ,mBAAmB;EACnB,YAAY;EACZ,aAAa;CACd;AACD;EACE,mBAAmB;EACnB,UAAU;EACV,QAAQ;EACR,6DAA6D;EAC7D,uBAAuB;EACvB,iBAAiB;EACjB,aAAa;CACd;AACD;EACE,+DAA+D;CAChE;AACD;EACE,aAAa;CACd;AACD;EACE,SAAS;CACV;AACD;EACE,SAAS;EACT,YAAY;CACb;AACD;EACE,OAAO;CACR;AACD;EACE,YAAY;EACZ,YAAY;CACb;AACD;EACE,UAAU;CACX;AACD;EACE,WAAW;EACX,QAAQ;EACR,2CAA2C;EAC3C,uBAAuB;EACvB,iBAAiB;EACjB,iBAAiB;CAClB;AACD;EACE,WAAW;EACX,aAAa;EACb,OAAO;EACP,QAAQ;CACT;AACD;EACE,YAAY;EACZ,YAAY;EACZ,SAAS;EACT,QAAQ;EACR,iBAAiB;CAClB", "file": "vue-train-timetable.vue", "sourcesContent": [".__vue-train-timetbale {\n  font-family: helvetica, sans-serif;\n  position: relative;\n  font-weight: 500;\n  box-sizing: border-box;\n}\n.__vue-train-timetbale ul {\n  padding: 0;\n  margin: 0;\n}\n.__vue-train-timetbale ul li {\n  list-style: none;\n}\n.__vue-train-timetbale .timetable {\n  height: 100%;\n}\n.__vue-train-timetbale .start-point,\n.__vue-train-timetbale .end-point {\n  width: 12px;\n  height: 12px;\n  border-radius: 12px;\n  position: absolute;\n  left: 0;\n  margin-left: -3px;\n}\n.__vue-train-timetbale .start-point {\n  top: -6px;\n}\n.__vue-train-timetbale .end-point {\n  bottom: -6px;\n}\n.__vue-train-timetbale .stop-item {\n  padding: 5px 0 5px 20px;\n  box-sizing: border-box;\n  position: relative;\n  height: 25px;\n}\n.__vue-train-timetbale .stop-bar {\n  position: absolute;\n}\n.__vue-train-timetbale .bearing-wall {\n  position: relative;\n  display: block;\n  visibility: hidden;\n  width: 0.1px;\n  text-indent: -1000px;\n  word-break: break-all;\n  line-height: 0.6;\n}\n.__vue-train-timetbale.horizontal {\n  height: 100%;\n}\n.__vue-train-timetbale.horizontal .start-point {\n  top: -4px;\n}\n.__vue-train-timetbale.horizontal .stop-bar {\n  width: 100%;\n  height: 4px;\n  bottom: 0px;\n  left: 0;\n}\n.__vue-train-timetbale.horizontal .stop-bar.pointing {\n  bottom: 0;\n  width: 4px;\n  height: 10px;\n  position: absolute;\n  left: 0;\n}\n.__vue-train-timetbale.horizontal .stop-item {\n  float: left;\n  position: relative;\n  width: 50px;\n  height: 100%;\n}\n.__vue-train-timetbale.horizontal .stop-text {\n  position: absolute;\n  bottom: 0;\n  left: 0;\n  transform: rotateZ(-45deg) translateY(-5px) translateX(10px);\n  transform-origin: left;\n  text-align: left;\n  width: 140px;\n}\n.__vue-train-timetbale.horizontal .departure .stop-text {\n  transform: rotateZ(-45deg) translateY(-15px) translateX(-10px);\n}\n.__vue-train-timetbale.horizontal .end-point {\n  bottom: -3px;\n}\n.__vue-train-timetbale.horizontal .terminal .stop-bar {\n  width: 0;\n}\n.__vue-train-timetbale.vertical .stop-item.start .stop-bar {\n  top: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale.vertical .stop-item.start .stop-bar .pointing {\n  top: 0;\n}\n.__vue-train-timetbale.vertical .stop-item.end .stop-bar {\n  bottom: 50%;\n  height: 50%;\n}\n.__vue-train-timetbale.vertical .stop-item.end .stop-bar .pointing {\n  top: 100%;\n}\n.__vue-train-timetbale.vertical .stop-item .departure .stop-text {\n  top: -50px;\n  left: 0;\n  transform: rotateZ(20deg) translateX(-30%);\n  transform-origin: left;\n  max-width: 140px;\n  text-align: left;\n}\n.__vue-train-timetbale.vertical .stop-bar {\n  width: 5px;\n  height: 100%;\n  top: 0;\n  left: 0;\n}\n.__vue-train-timetbale.vertical .stop-bar.pointing {\n  height: 4px;\n  width: 15px;\n  top: 50%;\n  left: 0;\n  margin-top: -2px;\n}\n"] }, media: undefined });
 };
 /* scoped */
-var __vue_scope_id__ = "data-v-f0b15d8a";
+var __vue_scope_id__ = "data-v-c487f6fa";
 /* module identifier */
 var __vue_module_identifier__ = undefined;
 /* functional template */
